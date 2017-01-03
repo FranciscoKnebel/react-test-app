a9781e94b7ff8e06201b6c1d1fb78638d058692a
Francisco <franciscopaivaknebel@gmail.com>
2017-01-03 20:48:30 -0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+				{
+					React.createElement(
+					'p',
+					{ className:'App-intro'},
+					'To get started, edit ',
+					React.createElement('code', null, 'src/App.js'),
+					' and save to reload.'
+				)}
+      </div>
+    );
+  }
+}
+
+export default App;
